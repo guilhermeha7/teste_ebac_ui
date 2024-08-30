01b9891b40403708beef65e958280fe1e8b2b383
Guilherme <gulhermeha77@outlook.com>
2024-08-30 17:01:23 -0300

    Primeiro caso de teste automatizado

---
+++ cypress/e2e/3-loja_ebac/login.cy.js
@@ -0,0 +1,15 @@
+
+/// <reference types="cypress"/>
+
+describe('AC1: Após inserir dados válidos o usuário deve ser direcionado para a tela "Minha Página"', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('gha@teste.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+
+        //Resultado Esperado
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gha (não é gha? Sair)')
+    })
+})
