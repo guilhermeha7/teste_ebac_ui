a31db6935b9a72cd6444d805726b65ae0d6045d1
Guilherme <gulhermeha77@outlook.com>
2024-08-31 01:09:29 -0300

    Alteração do nome do CT1 do AC1

--- cypress/e2e/3-loja_ebac/login.cy.js
+++ cypress/e2e/3-loja_ebac/login.cy.js
@@ -2,8 +2,8 @@
 /// <reference types="cypress"/>
 
 describe('AC1: Após inserir dados válidos o usuário deve ser direcionado para a tela "Minha Página"', () => {
-    
-    it('Deve fazer login com sucesso', () => {
+
+    it('Login com email e senha válidos', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('gha@teste.com')
         cy.get('#password').type('teste123')
@@ -12,4 +12,5 @@ describe('AC1: Após inserir dados válidos o usuário deve ser direcionado para
         //Resultado Esperado
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gha (não é gha? Sair)')
     })
+
 })
