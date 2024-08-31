e018032363030dc08f52bc0377451d56e3dd76be
Guilherme <gulhermeha77@outlook.com>
2024-08-31 15:10:24 -0300

    Add +1 cenário de teste automatizado

--- cypress/e2e/3-loja_ebac/login.cy.js
+++ cypress/e2e/3-loja_ebac/login.cy.js
@@ -3,8 +3,12 @@
 
 describe('AC1: Após inserir dados válidos o usuário deve ser direcionado para a tela "Minha Página"', () => {
 
-    it('Login com email e senha válidos', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Login com usuário e senha válidos', () => {
+        //Passo a Passo
         cy.get('#username').type('gha@teste.com')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
@@ -12,5 +16,29 @@ describe('AC1: Após inserir dados válidos o usuário deve ser direcionado para
         //Resultado Esperado
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gha (não é gha? Sair)')
     })
+
+describe('AC2: Após inserir um campo inválido deve exibir uma mensagem de erro', () => {
+    
+    it('Login com email inválido e senha válida', () => {
+        //Passo a Passo
+        cy.get('#username').type('rqwerqwrwqer@teste.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        
+        //Resultado Esperado
+        cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido.')
+    });
+
+    it('Login com email válido e senha inválida', () => {
+        //Passo a Passo
+        cy.get('#username').type('gha@teste.com')
+        cy.get('#password').type('rwewrewrewre')
+        cy.get('.woocommerce-form > .button').click()
+        
+        //Resultado Esperado
+        cy.get('.woocommerce-error').should('contain','Erro: A senha fornecida para o e-mail gha@teste.com está incorreta')
+    });
+
+});
 
 })
